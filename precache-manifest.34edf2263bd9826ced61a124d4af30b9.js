4ac29e63287e537be7361cee694cf4d14be4b1e2
G710 <16511117+G710@users.noreply.github.com>
2019-11-03 20:01:11 +0100

    Updates

---
+++ precache-manifest.34edf2263bd9826ced61a124d4af30b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92a3738256f2c141dc64430bd00c19b",
+    "url": "/totally-not-taboo/index.html"
+  },
+  {
+    "revision": "49f0783c1e95fdc38f52",
+    "url": "/totally-not-taboo/static/css/main.5fa24813.chunk.css"
+  },
+  {
+    "revision": "ca925832d19bdaa85bbf",
+    "url": "/totally-not-taboo/static/js/2.12837923.chunk.js"
+  },
+  {
+    "revision": "49f0783c1e95fdc38f52",
+    "url": "/totally-not-taboo/static/js/main.0a707826.chunk.js"
+  },
+  {
+    "revision": "54c612f2f1dd564d1f1f",
+    "url": "/totally-not-taboo/static/js/runtime-main.9824dbae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5413b8ff74567db088cd880aa0dca854.js"
+  "/totally-not-taboo/precache-manifest.34edf2263bd9826ced61a124d4af30b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/totally-not-taboo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
